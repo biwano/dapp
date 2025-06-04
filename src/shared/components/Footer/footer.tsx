56c06b980f67b41b93ec62b5e8c20bd9a96d9eda
biwano <biwano@gmail.com>
2025-06-04 15:31:52 +0200

    Fix footer

--- src/shared/components/Footer/footer.tsx
+++ src/shared/components/Footer/footer.tsx
@@ -7,15 +7,17 @@ import twitterIcon from './images/twitter.svg';
 export default function Footer() {
   return (
     <div className="flex flex-row background-void-80 text-white p-10 justify-between">
-      <div className="flex flex-row gap-10">
-        <div>KlimaProtocol.com</div>
+      <div className="flex flex-row gap-10 text-size-14">
+        <div className="text-green-40">KlimaProtocol.com</div>
         <div>Resources</div>
         <div>Disclaimer</div>
         <div>Contact</div>
       </div>
       <div className="flex flex-row gap-10">
         <div>
-          <div>© 2025 Klima Protocol. All rights reserved.</div>
+          <div className="text-size-12 text-void-20">
+            © 2025 Klima Protocol. All rights reserved.
+          </div>
         </div>
         <div className="flex flex-rox gap-4">
           <Image src={twitterIcon} alt="Twitter" />
